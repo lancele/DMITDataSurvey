034de19964bcb110ed8b295ef690a8d2a03faeef
Michelle Tang <msmetang@gmail.com>
2019-07-25 13:47:15 -0700

    disable button after the survey is submitted, added back code that i took out for testing

--- server.R
+++ server.R
@@ -36,7 +36,7 @@ server <- function(input, output, session){
   )
   for (i in seq_len(nrow(m))) {
     m[i, ] <- sprintf(
-      '<input type="radio" name="%s"" value="%s"/>',
+      '<input type="radio" name="%s" value="%s"/>',
       q5databases[i], m[i, ]
     )
   }
@@ -78,6 +78,54 @@ server <- function(input, output, session){
 
   observeEvent(input$submitResponse, {
     
+    ### Record responses and write to file
+    qNames <- c('q1','q2','q2other', 'q3', 'q4', paste('q5', letters[1:7], sep=''),
+                'q5other', 'q6name', 'q6answer', 'q7',
+                paste('q8', rep(letters[1:7], each = 3), 1:3, sep = ''), 'q8other',
+                paste('q9', rep(letters[1:9], each = 2), 1:2, sep = ''), 'q9other',
+                'q10', 'q11', 'SubmissionTime')
+    
+    response$response <- as.data.frame(matrix(nrow=1,ncol=length(qNames)))
+    
+    response$response[1, 1:5] <- c(input$q1, input$q2, input$q2other, input$q3, input$q4)
+    k <- 6
+    for(dbs in q5databases){
+      if(dbs%in%names(input)){
+        response$response[1, k] <- input[[dbs]]
+      }else{
+        next
+      }
+      k <- k+1
+    }
+    
+    
+    response$response[1, 13:16] <- c(input$q5other, input$q6name, input$q6answer, input$q7)
+    
+    k <- 17
+    for(subL in letters[1:7]){
+      for(subN in 1:3){
+        response$response[1, k] <- input[[paste('q8', subL, subN, sep='')]]
+        k <- k+1
+      }
+    }
+    response$response[1, 38] <- input$q8g
+    k <- 39
+    for(subL in letters[1:9]){
+      for(subN in 1:2){
+        response$response[1, k] <- input[[paste('q9', subL, subN, sep='')]]
+        k <- k+1
+      }
+    }
+    
+    response$response[1, 57] <- input$q9i
+    response$response[1, 58] <- input$q10
+    response$response[1, 59] <- input$q11
+    response$response[1, 60] <- as.character(Sys.time())
+    names(response$response) <- qNames
+    responseAll <- read.csv('responses.csv')
+    responseAll <- rbind(responseAll, response$response)
+    write.csv(responseAll, 'responses.csv', row.names=F)
+    
     
     
     ### Show modal dialog to confirm submission ###
@@ -90,15 +138,20 @@ server <- function(input, output, session){
       ))
     })
     
-    ### Clear all input after submission ###
+    ### Reset all input after submission ###
     reset('basicInfoForm')
     reset('dataUseForm')
     reset('dataSkillsForm')
     reset('addCommentsForm')
-    ### Clear radio buttons (Q5)
+    
+    ### Reset all radio buttons (for Q5)
     shinyjs::runjs(
       '$("[type=radio]").prop("checked", false);'
     )
+    
+    ### Disable submit button - to deter spam submissions ###
+    shinyjs::disable("submitResponse")
+    
   })
   
   #### end server code ####
